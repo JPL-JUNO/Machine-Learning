0207ffd57f8370bb02adf81eedd272cb3f77de73
sunyardCUI <1924375084@qq.com>
2023-06-15 09:50:44 +0800

    Evaluating and tuning the ensemble classifier

--- MLPS/Codes/Ch07/Codes7_2.py
+++ MLPS/Codes/Ch07/Codes7_2.py
@@ -4,8 +4,11 @@
 @LastEditor(s): Stephen CUI
 @CreatedTime: 2023-06-14 21:15:56
 """
-
+import sys
+sys.path.append('./')
+sys.path.append('../')
 import numpy as np
+import matplotlib.pyplot as plt
 np.argmax(np.bincount([0, 0, 1], weights=[.2, .2, .6]))
 
 ex = np.array([[.9, .1],
@@ -47,3 +50,80 @@ for clf, label in zip([pipe1, clf2, pipe3], clf_labels):
     scores = cross_val_score(estimator=clf, X=X_train,
                              y=y_train, cv=10, scoring='roc_auc')
     print(f'ROC AUC: {scores.mean():.2f} (+/-{scores.std():.2f}) [{label}]')
+
+from modelsNN.MajorityVoteClassifier import MajorityVoteEstimator
+mv_clf = MajorityVoteEstimator(classifiers=[pipe1, clf2, pipe3])
+clf_labels += ['Majority Voting']
+all_clf = [pipe1, clf2, pipe3, mv_clf]
+for clf, label in zip(all_clf, clf_labels):
+    scores = cross_val_score(estimator=clf, X=X_train,
+                             y=y_train, cv=10, scoring='roc_auc')
+    print(f'ROC AUC: {scores.mean():.2f} (+/-{scores.std():.2f}) [{label}]')
+
+# Evaluating and tuning the ensemble classifier
+from sklearn.metrics import roc_curve, auc
+colors = ['black', 'orange', 'blue', 'green']
+linestyles = [':', '--', '-.', '-']
+for clf, label, clr, ls in zip(all_clf, clf_labels, colors, linestyles):
+    y_pred = clf.fit(X_train, y_train).predict_proba(X_test)[:, 1]
+    fpr, tpr, thresholds = roc_curve(y_true=y_test, y_score=y_pred)
+    roc_auc = auc(x=fpr, y=tpr)
+    plt.plot(fpr, tpr, color=clr, linestyle=ls,
+             label=f'{label} (auc={roc_auc: .2f})')
+plt.legend()
+plt.plot([0, 1],
+         [0, 1], linestyle='--', color='gray', linewidth=2)
+plt.xlim([-.1, 1.1])
+plt.ylim([-.1, 1.1])
+plt.grid(alpha=0.5)
+plt.xlabel('False positive rate (FPR)')
+plt.ylabel('True positive rate (TPR)')
+plt.show()
+
+sc = StandardScaler()
+X_train_std = sc.fit_transform(X_train)
+from itertools import product
+x_min = X_train_std[:, 0].min() - 1
+x_max = X_train_std[:, 0].max() + 1
+y_min = X_train_std[:, 1].min() - 1
+y_max = X_train_std[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, .1),
+                     np.arange(y_min, y_max, .1))
+f, axarr = plt.subplots(nrows=2, ncols=2, figsize=(
+    7, 5), sharex='col', sharey='row')
+for idx, clf, tt in zip(product([0, 1], [0, 1]), all_clf, clf_labels):
+    clf.fit(X_train_std, y_train)
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    axarr[idx[0], idx[1]].contourf(xx, yy, Z, alpha=.3)
+    axarr[idx[0], idx[1]].scatter(
+        X_train_std[y_train == 0, 0], X_train_std[y_train == 0, 1], c='blue', marker='^', s=50)
+    axarr[idx[0], idx[1]].scatter(
+        X_train_std[y_train == 1, 0], X_train_std[y_train == 1, 1], c='green', marker='o', s=50)
+    axarr[idx[0], idx[1]].set_title(tt)
+plt.text(-3.5, -5.,
+         s='Sepal width [standardized]',
+         ha='center', va='center', fontsize=12)
+plt.text(-12.5, 4.5,
+         s='Petal length [standardized]',
+         ha='center', va='center',
+         fontsize=12, rotation=90)
+plt.show()
+
+
+mv_clf.get_params()
+from sklearn.model_selection import GridSearchCV
+params = {'decisiontreeclassifier__max_depth': [1, 2],
+          'pipeline-1__clf__C': [.001, .1, 100]}
+grid = GridSearchCV(estimator=mv_clf, param_grid=params,
+                    cv=10, scoring='roc_auc')
+grid.fit(X_train, y_train)
+
+for r, _ in enumerate(grid.cv_results_['mean_test_score']):
+    mean_score = grid.cv_results_['mean_test_score'][r]
+    std_dev = grid.cv_results_['std_test_score'][r]
+    params = grid.cv_results_['params'][r]
+    print(f'{mean_score:.3f} +/- {std_dev:.3f} +/- {params}')
+
+print(f'Best parameters: {grid.best_params_}')
+print(f'ROC AUC: {grid.best_score_:.2f}')
