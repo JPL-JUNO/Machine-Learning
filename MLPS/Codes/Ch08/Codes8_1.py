69fababc88cf0b4368745ad7a84f9b7cf1281764
sunyardCUI <1924375084@qq.com>
2023-06-19 18:02:24 +0800

    work with bigger data

--- MLPS/Codes/Ch08/Codes8_1.py
+++ MLPS/Codes/Ch08/Codes8_1.py
@@ -1,5 +1,5 @@
 """
-@Description: 
+@Description: Preparing the IMDb movie review data for text processing
 @Author(s): Stephen CUI
 @LastEditor(s): Stephen CUI
 @CreatedTime: 2023-06-16 20:16:18
@@ -9,13 +9,35 @@ import pyprind
 import pandas as pd
 import os
 import sys
+from packaging import version
+
 base_path = 'aclImdb'
 labels = {'pos': 1, 'neg': 0}
-pbar = pyprind.ProgBar(50_000, stream=sys.stdout)
+p_bar = pyprind.ProgBar(50_000, stream=sys.stdout)
 df = pd.DataFrame()
 for s in ('test', 'train'):
-    for l in ('pos', 'neg',):
+    for l in ('pos', 'neg'):
         path = os.path.join(base_path, s, l)
         for file in sorted(os.listdir(path)):
-            with open(os.path.join(path, file), 'r', encoding='utf8') as infile:
-                txt = infile.read()
+            with open(os.path.join(path, file), 'r', encoding='utf-8') as in_file:
+                txt = in_file.read()
+            if version.parse(pd.__version__) >= version.parse('1.3.2'):
+                x = pd.DataFrame([[txt, labels[l]]], columns=[
+                                 'review', 'sentiment'])
+                df = pd.concat([df, x], ignore_index=False)
+            else:
+                df = df.append([[txt, labels[l]]], ignore_index=True)
+            p_bar.update()
+df.columns = ['review', 'sentiment']
+
+
+import numpy as np
+np.random.seed(0)
+df = df.reindex(np.random.permutation(df.index))
+df.to_csv('movie_data.csv', index=False, encoding='utf-8')
+
+df = pd.read_csv('movie_data.csv', encoding='utf-8')
+df = df.rename(columns={'0': 'review', '1': 'sentiment'})
+df.sample(3)
+
+assert df.shape == (50_000, 2)
--- MLPS/Codes/utilsML/funcs.py
+++ MLPS/Codes/utilsML/funcs.py
@@ -6,6 +6,7 @@
 """
 from scipy.special import comb
 import math
+import re
 
 
 def ensemble_error(n_classifier: int, error: float):
@@ -15,3 +16,37 @@ def ensemble_error(n_classifier: int, error: float):
     probs = [comb(n_classifier, k) * error**k * (1 - error) **
              (n_classifier - k) for k in range(k_start, n_classifier + 1)]
     return sum(probs)
+
+
+def preprocessor(text: str) -> list:
+    text = re.sub('<[^>]*>', '', text)  # remove all of th HTML markup
+    emotions = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)', text)
+    text = (re.sub('[\W]+', ' ', text.lower()) +
+            ' '.join(emotions).replace('-', ''))
+    return text
+
+
+def tokenizer(text: str) -> list:
+    return text.split()
+
+
+from nltk.stem.porter import PorterStemmer
+porter = PorterStemmer()
+
+
+def tokenizer_porter(text: str) -> list:
+    return [porter.stem(word) for word in text.split()]
+
+
+from nltk.corpus import stopwords
+stop = stopwords.words('english')
+
+
+def tokenizer2(text: str) -> list:
+    text = preprocessor(text)
+    tokenized = [w for w in text.split() if w not in stop]
+    return tokenized
+
+
+if __name__ == '__main__':
+    pass
