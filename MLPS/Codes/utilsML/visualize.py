08533923ad07774f084bf252688565d8aaa7f24f
Stephen CUI <junocuixuan@163.com>
2023-06-25 22:34:47 +0800

    residual plot

--- MLPS/Codes/utilsML/visualize.py
+++ MLPS/Codes/utilsML/visualize.py
@@ -86,5 +86,27 @@ def lin_reg_plot(X, y, model, ax: Axes):
     ax.plot(X, model.predict(X), color='black', lw=2)
 
 
+def plot_residual(X_train, y_train, X_test, y_test, model):
+    y_train_pred = model.predict(X_train)
+    y_test_pred = model.predict(X_test)
+
+    x_max = np.max([np.max(y_train_pred), np.max(y_test_pred)])
+    x_min = np.min([np.min(y_train_pred), np.min(y_test_pred)])
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(7, 3), sharey=True)
+    ax1.scatter(y_test_pred, y_test_pred - y_test, c='limegreen', marker='s', edgecolor='white',
+                label='Test data')
+    ax2.scatter(y_train_pred, y_train_pred - y_train, c='steelblue', marker='o', edgecolor='white',
+                label='Training data')
+    ax1.set_ylabel('Residuals')
+    for ax in (ax1, ax2):
+        ax.set_xlabel('Predicted values')
+        ax.legend()
+        ax.hlines(y=0, xmin=x_min - 100, xmax=x_max + 100,
+                  color='black', lw=2)
+    plt.tight_layout()
+    plt.show()
+
+
 if __name__ == '__main__':
     pass
