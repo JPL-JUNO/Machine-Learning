a0143f4e49c3ce0f04c93c60d42746db1278ee9f
sunyardCUI <1924375084@qq.com>
2023-07-05 17:36:00 +0800

    torch

---
+++ MLPS/Codes/Ch12/Building_an_NN_model_in_PyTorch.py
@@ -0,0 +1,55 @@
+"""
+@Description: Building an NN model in PyTorch
+@Author(s): Stephen CUI
+@LastEditor(s): Stephen CUI
+@CreatedTime: 2023-07-05 17:12:20
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+X_train = np.arange(10, dtype='float32').reshape((10, 1))
+y_train = np.array([1.0, 1.3, 3.1, 2.0, 5.0,
+                    6.3, 6.6, 7.4, 8.0, 9.0], dtype='float32')
+plt.plot(X_train, y_train, 'o', markersize=7)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
+
+from torch.utils.data import TensorDataset, DataLoader
+X_train_norm = (X_train - np.mean(X_train)) / np.std(X_train)
+X_train_norm = torch.from_numpy(X_train_norm)
+y_train = torch.from_numpy(y_train)
+train_ds = TensorDataset(X_train_norm, y_train)
+batch_size = 1
+train_dl = DataLoader(train_ds, batch_size, shuffle=True)
+
+torch.manual_seed(1)
+weight = torch.randn(1)
+weight.requires_grad_()
+bias = torch.zeros(1, requires_grad=True)
+
+
+def model(xb):
+    return xb @ weight + bias
+
+
+def loss_fn(input, target) -> float:
+    return (input - target).pow(2).mean()
+
+
+learning_rate = .001
+num_epochs = 200
+log_epochs = 10
+
+for epoch in range(num_epochs):
+    for x_batch, y_batch in train_dl:
+        pred = model(x_batch)
+        loss = loss_fn(pred, y_batch)
+        loss.backward()
+    with torch.no_grad():
+        weight -= weight.grad * learning_rate
+        bias -= bias.grad * learning_rate
+        weight.grad.zero_()
+        bias.grad.zero_()
+    if epoch % log_epochs == 0:
+        print(f'Epoch {epoch} Loss {loss.item():.4f}')
