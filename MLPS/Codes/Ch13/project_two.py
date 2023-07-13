4dcca98c6fb041b77c9dd24f8afac121e3aaf423
Stephen CUI <junocuixuan@163.com>
2023-07-13 21:03:51 +0800

    mnist handwritten digits

--- MLPS/Codes/Ch13/project_two.py
+++ MLPS/Codes/Ch13/project_two.py
@@ -5,24 +5,65 @@
 @CreatedTime: 2023-07-13 17:19:55
 """
 
+import os
 import torchvision
 from torchvision import transforms
 import torch
+import torch.nn as nn
 from torch.utils.data import DataLoader
 image_path = './'
 transform = transforms.Compose([
     transforms.ToTensor()
 ])
-
+# 修改为判断文件夹是否存在，不在手动改下面的download参数
+download_flg = False if os.path.exists('MNIST') else True
 # 首次需要将download设置为True
 mnist_train_dataset = torchvision.datasets.MNIST(
     root=image_path, train=True,
-    transform=transform, download=False
+    transform=transform, download=download_flg
 )
 mnist_test_dataset = torchvision.datasets.MNIST(
-    root=image_path, train=False, transform=transform, download=False
+    root=image_path, train=False, transform=transform, download=download_flg
 )
 
 batch_size = 64
 torch.manual_seed(1)
 train_dl = DataLoader(mnist_train_dataset, batch_size, shuffle=True)
+
+# mnist_train_dataset[index]是一个元组，(数据,标签)
+# mnist_train_dataset.data can access the raw pixels
+# mnist_train_dataset[0][0]
+
+hidden_units = [32, 16]
+image_size = mnist_test_dataset[0][0].shape
+input_size = image_size[0] * image_size[1] * image_size[2]
+all_layers = [nn.Flatten()]
+for hidden_unit in hidden_units:
+    layer = nn.Linear(input_size, hidden_unit)
+    all_layers.append(layer)
+    all_layers.append(nn.ReLU())
+    input_size = hidden_unit
+all_layers.append(nn.Linear(hidden_units[-1], 10))
+model = nn.Sequential(*all_layers)
+print(model)
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=.001)
+torch.manual_seed(1)
+num_epochs = 20
+for epoch in range(num_epochs):
+    accuracy_hist_train = 0
+    for x_batch, y_batch in train_dl:
+        pred = model(x_batch)
+        loss = loss_fn(pred, y_batch)
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+        is_correct = (torch.argmax(pred, dim=1) == y_batch).float()
+        accuracy_hist_train += is_correct.sum()
+    accuracy_hist_train /= len(train_dl.dataset)
+    print(f'Epoch {epoch:2d} Accuracy {accuracy_hist_train:.4f}')
+
+pred = model(mnist_test_dataset.data / 255.)
+is_correct = (torch.argmax(pred, dim=1) == mnist_test_dataset.targets).float()
+print(f'Test accuracy: {is_correct.mean():.4f}')
