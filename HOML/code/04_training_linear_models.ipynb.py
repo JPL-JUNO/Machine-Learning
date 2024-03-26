1475b7b3f4c46f28b4782f0cc443d4e670c06fd4
Stephen CUI <junocuixuan@163.com>
2024-03-26 22:43:24 +0800

    逻辑回归，决策边界，softmax 回归

--- HOML/code/04_training_linear_models.ipynb.py
+++ HOML/code/04_training_linear_models.ipynb.py
@@ -16,6 +16,8 @@ import sklearn
 assert version.parse(sklearn.__version__) >= version.parse("1.0.1")
 
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
 plt.rc("font", size=14)
 plt.rc("axes", labelsize=14, titlesize=14)
@@ -151,4 +153,148 @@ plt.ylabel("RMSE")
 plt.grid()
 plt.axis([0, 80, 0, 2.5])
 save_fig("learning_curves_plot")
+plt.show()
+
+lim = 6
+t = np.linspace(-lim, lim, 100)
+sig = 1 / (1 + np.exp(-t))
+
+plt.figure(figsize=(8, 3))
+plt.plot([-lim, lim], [0, 0], "k-")
+plt.plot([-lim, lim], [0.5, 0.5], "k:")
+plt.plot([-lim, lim], [1, 1], "k:")
+plt.plot([0, 0], [-1.1, 1.1], "k-")
+plt.plot(t, sig, "b-", linewidth=2, label=r"$\sigma(t) = \dfrac{1}{1 + e^{-t}}$")
+plt.xlabel("t")
+plt.legend(loc="upper left")
+plt.axis([-lim, lim, -0.1, 1.1])
+plt.gca().set_yticks([0, 0.25, 0.5, 0.75, 1])
+plt.grid()
+save_fig("logistic_function_plot")
+plt.show()
+
+# 决策边界
+from sklearn.datasets import load_iris
+
+iris = load_iris(as_frame=True)
+
+X = iris.data[["petal width (cm)"]].values
+y = iris.target_names[iris.target] == "virginica"
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+log_reg = LogisticRegression(random_state=42)
+log_reg.fit(X_train, y_train)
+
+# reshape to get a column vector
+X_new = np.linspace(0, 3, 100).reshape(-1, 1)
+y_proba = log_reg.predict_proba(X_new)
+decision_boundary = X_new[y_proba[:, 1] >= 0.5][0, 0]
+
+plt.figure(figsize=(8, 3))
+plt.plot(X_new, y_proba[:, 0], "b--", linewidth=2, label="Not Iris virginica proba")
+plt.plot(X_new, y_proba[:, 1], "g-", linewidth=2, label="Iris virginica proba")
+plt.plot(
+    [decision_boundary, decision_boundary],
+    [0, 1],
+    "k:",
+    linewidth=2,
+    label="Decision boundary",
+)
+
+plt.arrow(
+    x=decision_boundary,
+    y=0.08,
+    dx=-0.3,
+    dy=0,
+    head_width=0.05,
+    head_length=0.1,
+    fc="b",
+    ec="b",
+)
+plt.arrow(
+    x=decision_boundary,
+    y=0.92,
+    dx=0.3,
+    dy=0,
+    head_width=0.05,
+    head_length=0.1,
+    fc="g",
+    ec="g",
+)
+plt.plot(X_train[y_train == 0], y_train[y_train == 0], "bs")
+plt.plot(X_train[y_train == 1], y_train[y_train == 1], "g^")
+plt.xlabel("")
+plt.ylabel("")
+plt.legend()
+plt.axis([0, 3, -0.02, 1.02])
+plt.grid()
+save_fig("logistic_regression_plot")
+plt.show()
+
+X = iris.data[["petal length (cm)", "petal width (cm)"]].values
+y = iris.target_names[iris.target] == "virginica"
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+
+log_reg = LogisticRegression(C=2, random_state=42)
+log_reg.fit(X_train, y_train)
+
+x0, x1 = np.meshgrid(
+    np.linspace(2.9, 7, 500).reshape(-1, 1), np.linspace(0.8, 2.7, 200).reshape(-1, 1)
+)
+X_new = np.c_[x0.ravel(), x1.ravel()]
+y_proba = log_reg.predict_proba(X_new)
+zz = y_proba[:, 1].reshape(x0.shape)
+
+left_right = np.array([2.9, 7])
+boundary = -(
+    (log_reg.coef_[0, 0] * left_right + log_reg.intercept_[0]) / log_reg.coef_[0, 1]
+)
+
+plt.figure(figsize=(10, 4))
+plt.plot(X_train[y_train == 0, 0], X_train[y_train == 0, 1], "bs")
+plt.plot(X_train[y_train == 1, 0], X_train[y_train == 1, 1], "r^")
+contour = plt.contour(x0, x1, zz, cmap=plt.cm.brg)
+plt.clabel(contour, inline=1)
+plt.plot(left_right, boundary, "k--", linewidth=3)
+plt.text(3.5, 1.27, "Not Iris virginica", color="b", ha="center")
+plt.text(6.5, 2.3, "Iris virginica", color="g", ha="center")
+plt.xlabel("Petal length")
+plt.ylabel("Petal width")
+plt.axis([2.9, 7, 0.8, 2.7])
+plt.grid()
+save_fig("logistic_regression_contour_plot")
+plt.show()
+
+## Softmax Regression
+X = iris.data[["petal length (cm)", "petal width (cm)"]].values
+y = iris["target"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+softmax_reg = LogisticRegression(C=30, random_state=42)
+softmax_reg.fit(X, y)
+
+from matplotlib.colors import ListedColormap
+
+custom_cmap = ListedColormap(["#fafab0", "#9898ff", "#a0faa0"])
+x0, x1 = np.meshgrid(
+    np.linspace(0, 8, 500).reshape(-1, 1), np.linspace(0, 3.5, 200).reshape(-1, 1)
+)
+X_new = np.c_[x0.ravel(), x1.ravel()]
+y_proba = softmax_reg.predict_proba(X_new)
+y_predict = softmax_reg.predict(X_new)
+
+zz1 = y_proba[:, 1].reshape(x0.shape)
+zz = y_predict.reshape(x0.shape)
+
+plt.figure(figsize=(10, 4))
+plt.plot(X[y == 2, 0], X[y == 2, 1], "g^", label="Iris virginica")
+plt.plot(X[y == 1, 0], X[y == 1, 1], "bs", label="Iris versicolor")
+plt.plot(X[y == 0, 0], X[y == 0, 1], "yo", label="Iris setosa")
+plt.contourf(x0, x1, zz, cmap=custom_cmap)
+contour = plt.contour(x0, x1, zz1, cmap="hot")
+plt.clabel(contour, inline=1)
+plt.xlabel("Petal length")
+plt.ylabel("Petal width")
+plt.legend(loc="center left")
+plt.axis([0.5, 7, 0, 3.5])
+plt.grid()
+save_fig("softmax_regression_contour_plot")
 plt.show()
